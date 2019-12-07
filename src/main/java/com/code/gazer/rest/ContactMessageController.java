74c8f8a6be5d978e98b5d0ca09f0c6212e88824a
Dmitry Popov <dmitry.popov@godel.shellenergy.co.uk>
2019-12-07 19:08:50 +0300

    add contact message controller

---
+++ src/main/java/com/code/gazer/rest/ContactMessageController.java
@@ -0,0 +1,19 @@
+package com.code.gazer.rest;
+
+import com.code.gazer.domain.Message;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ContactMessageController {
+
+  @PostMapping("/message")
+  public ResponseEntity<Message> postMessage(@RequestBody final Message message) {
+    System.out.println(message);
+
+    return ResponseEntity.ok(message);
+  }
+
+}
